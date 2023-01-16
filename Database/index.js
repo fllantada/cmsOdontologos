933d8790f49ff9d628bc7863e9528c606329d3e6
Marianela <84550042+MarianelaNovelli@users.noreply.github.com>
2023-01-16 18:43:55 -0300

    Carga - Docker

--- Database/index.js
+++ Database/index.js
@@ -1,9 +1,47 @@
-const MongoClient = require('mongodb').MongoClient;
+import { MongoClient } from "mongodb";
+import { faker } from "@faker-js/faker";
 
-const uri = "mongodb+srv://usuarioprueba:ImMo9NtNl1cdbWeu@cluster0.iuzwtmv.mongodb.net/?retryWrites=true&w=majority";
+async function start() {
+  const db = await connectMongo();
+  const randomData = createRandomData(100);
+  const dentistas = db.collection("dentistas");
+  await dentistas.insertMany(randomData);
+  process.exit(0);
+}
 
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  if(err) throw err;
-    console.log("Conectado a MongoDB");
-  });
+start();
+
+export async function connectMongo() {
+  const url = "mongodb://localhost:27017";
+  try {
+    const client = await MongoClient.connect(url, { useNewUrlParser: true });
+
+    const db = client.db("DBprueba");
+    return db;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function createRandomData(n) {
+  const dentistas = [];
+
+  for (let i = 0; i < n; i++) {
+    dentistas.push({
+      idSucursal: faker.finance.account(8),
+      rut: faker.finance.account(3),
+      nombre: faker.name.fullName(),
+      apellido: faker.name.lastName(),
+      telefono: faker.phone.imei(),
+      email: faker.internet.email(),        
+      direccion: faker.address.buildingNumber(),
+      nombreusuario: faker.name.fullName(),
+      password: faker.random.alpha(8),
+      agendaOnline: faker.random.numeric(1),
+      ciudad: faker.address.cityName(),        
+      duracionConsulta: faker.finance.account(1),
+    });
+  }
+
+  return dentistas;
+}
--- Database/restart.sh
+++ Database/restart.sh
@@ -1,24 +1,12 @@
-# Verifica si la carpeta /data/db existe en el host
 if [ ! -d "./data" ]; then
-  # Crea la carpeta /data/db en el host
-  echo "Creando carpeta /data en el host"
   mkdir data
 fi
 
-docker stop contenedor_mongo_docker
-docker rm contenedor_mongo_docker
-docker rmi imagen_mongo_mockdata
-echo "Termine el stop.sh"
+docker stop db_dentistas_container
+docker rm db_dentistas_container
+docker rmi db_dentistas_image
 
-#Contenedor: docker run -d --name contenedor-mongo-docker -p 8080:8080 mongo:latest
+docker build db_dentistas_image .
 
-# Construye la imagen de Docker
-docker build -t imagen_mongo_mockdata .
-echo "Termine el build.sh"
-
-# Ejecuta un contenedor de Docker con la carpeta /data/db montada como volumen
-docker run -d -p 27017:27017 -v "$(pwd)/data:/data" --name contenedor_mongo_docker imagen_mongo_mockdata
-echo "Termine el run.sh"
-
-#cargar los datos
-docker exec -it contenedor_mongo_docker mongoimport --db pedro --collection datos --file /data/usuarios.json --jsonArray --upsert --upsertFields email
+docker run -d -p 27017:27017 -v "$(pwd)/data:/data" --name db_dentistas_container db_dentistas_image
+echo "Carga exitosa"
