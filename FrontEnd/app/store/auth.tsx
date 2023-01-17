b3a66d5a4b53a240785937e716cb579811b000f5
fllantada <franciscollantada@gmail.com>
2023-01-17 19:56:06 -0300

    instrucciones store y servicios Auth

--- FrontEnd/app/store/auth.tsx
+++ FrontEnd/app/store/auth.tsx
@@ -1,10 +1,14 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
+//Contexto Auth
+// store  --> este estore
+// services --> getMockPerfilService (email:String) --> Promise<Perfil>
+
 export interface Perfil {
   email: string;
   role: "admin" | "user" | "guest";
-}
+} //se importa de entidades
 
 type State = {
   usuario: Perfil;
@@ -12,6 +16,7 @@ type State = {
 
 type Actions = {
   setPerfil: (usuario: Perfil) => void;
+  getPerfil: (email: string) => void;
 };
 
 export const useAuthStore = create(
@@ -19,9 +24,16 @@ export const useAuthStore = create(
     (set) => ({
       usuario: {} as Perfil,
       setPerfil: (usuario: Perfil) => set((state) => ({ ...state, usuario })),
+
+      getPerfil: (email: string) => {
+        //logica para setear un perfil desde getMockPerfilService
+      }, //consumir un servicio  en carpeta services que llame a una api y devuelva PErfil
     }),
+
     {
       name: "auth",
     }
   )
 );
+
+//
