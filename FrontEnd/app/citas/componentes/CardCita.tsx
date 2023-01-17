ecf0fa433c070d48cfd74ed5cd8db4d2060b69e2
Alicia Benítez Samaniego <benitez.ali@gmail.com>
2023-01-17 17:53:53 -0300

    mejorar cards

--- FrontEnd/app/citas/componentes/CardCita.tsx
+++ FrontEnd/app/citas/componentes/CardCita.tsx
@@ -1,29 +1,31 @@
-"use client";
+
 import { Cita } from "../domain/Cita.entity";
 import Alert from "@mui/material/Alert";
+import Grid from '@mui/material/Unstable_Grid2'; // Grid2
+import { Card } from '@mui/material';
 
 const CardCita: React.FC<{ cita: Cita }> = ({ cita }) => {
   return (
-    <div key={cita.id_paciente}>
-      <p>
-        Hora inicio:{cita.hora_inicio} a {cita.hora_fin}
-      </p>
-      <p>Hora Fin {cita.nombre_paciente}</p>
-      <h1>Dentista: {cita.nombre_dentista}</h1>
-      <h2 color='blue'>Estado {cita.nombre_estado}</h2>
-      <h1>
-        {" "}
-        {cita.id_situacion === 1 ? (
-          <Alert severity='error'>{cita.descripcion_situacion}</Alert>
-        ) : cita.id_situacion === 2 ? (
-          <Alert severity='warning'>{cita.descripcion_situacion}</Alert>
-        ) : cita.id_situacion === 3 ? (
-          <Alert severity='info'>{cita.descripcion_situacion}</Alert>
-        ) : (
-          <Alert severity='success'>{cita.descripcion_situacion}</Alert>
-        )}
-      </h1>
-    </div>
+    <Grid xs={3} spacing={1} >
+      <Card  key={cita.id_paciente}  >
+        <p>Hora: {cita.hora_inicio} a {cita.hora_fin} </p>
+        <p>Paciente: {cita.nombre_paciente}</p>
+        <p>Dentista: {cita.nombre_dentista}</p>
+        <h3>Estado: {cita.nombre_estado}</h3>
+        <h1>
+          {" "}
+          {cita.id_situacion === 1 ? (
+            <Alert severity='error'>{cita.descripcion_situacion}</Alert>
+          ) : cita.id_situacion === 2 ? (
+            <Alert severity='warning'>{cita.descripcion_situacion}</Alert>
+          ) : cita.id_situacion === 3 ? (
+            <Alert severity='info'>{cita.descripcion_situacion}</Alert>
+          ) : (
+            <Alert severity='success'>{cita.descripcion_situacion}</Alert>
+          )}
+        </h1>
+      </Card>
+    </Grid>
   );
 };
 export default CardCita;
